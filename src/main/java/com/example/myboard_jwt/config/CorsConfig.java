a18a8cff96b20209f9827b295c85aaa6e8a6a15e
ggongdori <soominlee0410@gmail.com>
2022-04-21 15:36:27 +0900

    exposedheader corsfilter 추가

--- src/main/java/com/example/myboard_jwt/config/CorsConfig.java
+++ src/main/java/com/example/myboard_jwt/config/CorsConfig.java
@@ -17,6 +17,7 @@ public class CorsConfig {
         config.addAllowedOrigin("*");
         config.addAllowedHeader("*");
         config.addAllowedMethod("*");
+        config.addExposedHeader("*");
 
         source.registerCorsConfiguration("/api/**", config);
         return new CorsFilter(source);
