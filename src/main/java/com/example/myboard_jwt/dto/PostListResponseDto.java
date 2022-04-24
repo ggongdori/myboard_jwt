baa25491b586abab3a9cdc8e1847f90d8e6c5ef9
ggongdori <soominlee0410@gmail.com>
2022-04-24 20:40:40 +0900

    PostresponseDto added(boardId, nickname, modifiedAt, likecount, boolean islike)

--- src/main/java/com/example/myboard_jwt/dto/PostListResponseDto.java
+++ src/main/java/com/example/myboard_jwt/dto/PostListResponseDto.java
@@ -1,15 +1,11 @@
 package com.example.myboard_jwt.dto;
 
-import com.example.myboard_jwt.entity.Post;
-import com.example.myboard_jwt.service.FileHandler;
-import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.domain.Slice;
 
-import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
