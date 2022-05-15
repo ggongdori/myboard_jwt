e572aec45f5c41e74a8dc36a96c5361d27688939
ggongdori <soominlee0410@gmail.com>
2022-05-15 23:58:43 +0900

    websocket initial

--- src/main/java/com/example/myboard_jwt/controller/UserController.java
+++ src/main/java/com/example/myboard_jwt/controller/UserController.java
@@ -38,6 +38,7 @@ public class UserController {
         return ResponseEntity.ok("hello");
     }
 
+
     @PostMapping("/test-redirect")
     public void testRedirect(HttpServletResponse response) throws IOException {
         response.sendRedirect("/api/user");
